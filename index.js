fe3344560bb9b74dc92d3158f36bd2e936501bad
EgorYakovenko <Egor.Yakovenko@ukr.net>
2025-04-07 21:53:30 +0200

    add token user

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { registerValidation } from "./validations/auth.js";
 import { validationResult } from "express-validator";
 
 import UserModel from "./models/User.js";
-//mongodb+srv://egoryakovenko:pRiojgjWKjmO2T58@cluster0.hyycetn.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0
+
 mongoose
   .connect(
     "mongodb+srv://egoryakovenko:pRiojgjWKjmO2T58@cluster0.hyycetn.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
@@ -22,27 +22,46 @@ const app = express();
 app.use(express.json());
 
 app.post("/auth/register", registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    // ======= шифрование пароля =======
+    const password = req.body.password; // получаем пароль из запроса req
+    const salt = await bcrypt.genSalt(10); // генерирум алгоритьм шифрования "salt"
+    const hash = await bcrypt.hash(password, salt); // шифрование
+    // ======= /шифрование пароля =======
+
+    //создание документа пользователя
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save(); //сохраняем пользователя в базе
+
+    //токен пользователя
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      { expiresIn: "30d" }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "не удалось зарегистрироватся",
+    });
   }
-
-  // ======= шифрование пароля =======
-  const password = req.body.password; // получаем пароль из запроса req
-  const salt = await bcrypt.genSalt(10); // генерирум алгоритьм шифрования "salt"
-  const passwordHash = await bcrypt.hash(password, salt); // шифрование
-  // ======= /шифрование пароля =======
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save(); //сохраняем пользователя в базе
-
-  res.json(user);
 });
 
 app.listen(4444, (err) => {
